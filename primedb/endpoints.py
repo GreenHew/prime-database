ba864f518ae5dd285ca6bb4b087cf3ce275e87bc
Matt <greenhewvid@gmail.com>
2019-12-30 15:25:23 -0800

    get nth prime

--- primedb/endpoints.py
+++ primedb/endpoints.py
@@ -1,2 +1,18 @@
+from primedb.services.s3_controller import S3Controller
+
+s3 = S3Controller()
 def example_endpoint(body):
     return {'status': 'success'}, 200
+
+
+def get_nth_prime(n, bucket_size=1000):
+    prime_index = n % bucket_size - 1
+    bucket_start = n - prime_index
+    key_name = 'nth_primes/{}/{}-{}.txt'.format(bucket_size, bucket_start, bucket_start + bucket_size - 1)
+    prime_string = s3.client.get_object(Bucket='primedatabase', Key=key_name)['Body'].read().decode('utf-8')
+    prime = prime_string.split(',')[prime_index]
+    return prime
+
+
+if __name__ == '__main__':
+    print(get_nth_prime(52592))
